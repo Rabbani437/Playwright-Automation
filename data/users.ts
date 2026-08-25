28cfa02068270eb65d57fba5cb83db85346ab09f
Rabbani437 <shaik.rabbani@wmlit.com>
2026-08-25 16:00:35 +0530

    login

---
+++ data/users.ts
@@ -0,0 +1,11 @@
+export const users = {
+    user1: {
+        username: 'shaikrabbani876@gmail.com',
+        password: 'Wml@12345'
+    },
+    user2:{
+        username:'wml@wmltech.com',
+        password:'Wml@1234'
+
+    }
+};
